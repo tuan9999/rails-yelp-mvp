1a01bb5b429668bc2b85589f7cf937f3bf21b260
tuan perera <tuanperera@gmail.com>
2018-02-14 16:14:32 +0100

    finished

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,46 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+puts 'Cleaning database...'
+Restaurant.destroy_all
+restaurants_attributes = [
+  {
+    name:         'Dishoom',
+    address:      '7 Boundary St, London E2 7JE',
+    phone_number:  '1223234523456',
+    category:        'italian'
+  },
+  {
+    name:         'fawer245',
+    address:      '7 Boundary St',
+    phone_number:  '1223234523456',
+    category:        'french'
+  },
+  {
+    name:         'kfc',
+    address:      'London E2 7JE',
+    phone_number:  '1223234523456',
+    category:        'chinese'
+  },
+  {
+    name:         'asasasas',
+    address:      '7 Boundary St, London E2 7JE',
+    phone_number:  '1223234523456',
+    category:        'belgian'
+  },
+  {
+    name:         'oioioioioio',
+    address:      '7 Boundary St, London E2 7JE',
+    phone_number:  '1223234523456',
+    category:        'chinese'
+  },
+  {
+    name:         'popoppkplplppok',
+    address:      '7 Boundary St, London E2 7JE',
+    phone_number:  '1223234523456',
+    category:        'belgian'
+  }
+]
+Restaurant.create!(restaurants_attributes)
+puts 'Finished!'
